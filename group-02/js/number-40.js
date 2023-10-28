6f666e8df58747813ee2fd5b36bbe2590fbccc4e
lehung2022 <hung.le.programmer@gmail.com>
2023-10-28 11:28:07 +0700

    57 questions have been done. Half of them are left

---
+++ group-02/js/number-40.js
@@ -0,0 +1,12 @@
+// Write a JavaScript function to create a random background color.
+function random_bg_color() {
+    var x = Math.floor(Math.random() * 256);
+    var y = Math.floor(Math.random() * 256);
+    var z = Math.floor(Math.random() * 256);
+    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+    console.log(bgColor);
+
+    document.body.style.background = bgColor;
+}
+
+random_bg_color();
